3cde5191387fe8bf19e0b4a742420e1e8fd4a77b
Kevin Dumont <kevindumont@Mac.lan>
2026-03-26 12:09:44 +0100

    Fix: CueRule rename + export CUE_COLOR_RGB - final fixes

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,5 @@
 from app.models.user import User
-from app.models.track import Track, TrackAnalysis, CuePoint, CueRule
+from app.models.track import Track, TrackAnalysis, CuePoint, CueRule, CUE_COLOR_RGB
 
-__all__ = ["User", "Track", "TrackAnalysis", "CuePoint", "CueRule"]
+__all__ = ["User", "Track", "TrackAnalysis", "CuePoint", "CueRule", "CUE_COLOR_RGB"]
 from app.models.subscription import Subscription  # noqa: F401
