baeee2589d2cc84860809a3bf0562b831525230c
kdumontm <k.dumont.m@gmail.com>
2026-03-26 16:30:52 +0100

    Add PageConfig import to models __init__

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.user import User
 from app.models.track import Track, TrackAnalysis, CuePoint, CueRule, CUE_COLOR_RGB
+from app.models.site_settings import PageConfig, DEFAULT_PAGES
 
-__all__ = ["User", "Track", "TrackAnalysis", "CuePoint", "CueRule", "CUE_COLOR_RGB"]
+__all__ = ["User", "Track", "TrackAnalysis", "CuePoint", "CueRule", "CUE_COLOR_RGB", "PageConfig", "DEFAULT_PAGES"]
 from app.models.subscription import Subscription  # noqa: F401
