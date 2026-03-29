c22f226beb1cf9276f1887416e87d6000e4d8e6c
kdumontm <k.dumont.m@gmail.com>
2026-03-29 11:54:47 +0200

    feat: export PlanFeature model from models package

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,6 @@
 from app.models.user import User
 from app.models.track import Track, TrackAnalysis, CuePoint, CueRule, CUE_COLOR_RGB
-from app.models.site_settings import PageConfig, DEFAULT_PAGES
+from app.models.site_settings import PageConfig, DEFAULT_PAGES, PlanFeature, DEFAULT_PLAN_FEATURES, DEFAULT_PLAN_CONFIGS
 
-__all__ = ["User", "Track", "TrackAnalysis", "CuePoint", "CueRule", "CUE_COLOR_RGB", "PageConfig", "DEFAULT_PAGES"]
+__all__ = ["User", "Track", "TrackAnalysis", "CuePoint", "CueRule", "CUE_COLOR_RGB", "PageConfig", "DEFAULT_PAGES", "PlanFeature", "DEFAULT_PLAN_FEATURES", "DEFAULT_PLAN_CONFIGS"]
 from app.models.subscription import Subscription  # noqa: F401
