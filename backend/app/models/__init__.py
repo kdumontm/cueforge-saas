8a8178b007f40753a244563d48d467c27f2be8bd
kdumontm <k.dumont.m@gmail.com>
2026-03-29 23:02:44 +0200

    feat: register Organization, OrgInvite, UsageLog models in __init__

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,11 @@
 from app.models.user import User
 from app.models.track import Track, TrackAnalysis, CuePoint, CueRule, CUE_COLOR_RGB
 from app.models.site_settings import PageConfig, DEFAULT_PAGES, PlanFeature, DEFAULT_PLAN_FEATURES, DEFAULT_PLAN_CONFIGS
-
-__all__ = ["User", "Track", "TrackAnalysis", "CuePoint", "CueRule", "CUE_COLOR_RGB", "PageConfig", "DEFAULT_PAGES", "PlanFeature", "DEFAULT_PLAN_FEATURES", "DEFAULT_PLAN_CONFIGS"]
+from app.models.organization import Organization, OrgInvite, UsageLog
 from app.models.subscription import Subscription  # noqa: F401
+
+__all__ = [
+    "User", "Track", "TrackAnalysis", "CuePoint", "CueRule", "CUE_COLOR_RGB",
+    "PageConfig", "DEFAULT_PAGES", "PlanFeature", "DEFAULT_PLAN_FEATURES", "DEFAULT_PLAN_CONFIGS",
+    "Organization", "OrgInvite", "UsageLog", "Subscription",
+]
